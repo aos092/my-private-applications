5e07562d7154a7a8227b673e30f9c1d1ce59b95e
abdullah <aos772627092@gmail.com>
2025-11-16 04:34:23 +0300

    container widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(const MyApp());
 }
-class MyApp extends StatelessWidget{
- @override
-  Widget build(BuildContext context){
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("MyFirst_app"),),
-        body: Text("Abdullah Subih" ,style:TextStyle(
-          color: Color.fromARGB(255, 224, 59, 16),
-          fontSize: 50,
-          fontWeight: FontWeight.bold,
-          backgroundColor: Colors.black
-        ),),
-      )
+        appBar: AppBar(title: const Text("container widget")),
+        body: Container(
+          decoration: BoxDecoration(
+            //alignment: Alignment.center,
+            color: Colors.teal,
+            borderRadius: const BorderRadius.all(Radius.circular(4000) ),
+            border: Border.all(color: Colors.black, width: 10),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.blue,
+                offset: Offset(1, 1),
+                spreadRadius: 1,
+                blurRadius: 10,
+              ),
+            ],
+          ),
+          width: 300,
+          height: 300,
+          alignment: Alignment.center,
+          margin: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(20),
+          child: const Text(
+            "Abdullah Subih",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.2,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
