a371af9f056b56f887718e66becef348d0fadb77
abdullah <aos772627092@gmail.com>
2026-01-24 18:50:45 +0300

    listTile

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text("container widget")),
+        appBar: AppBar(title: const Text("ListTile")),
         body: Container(
-          decoration: BoxDecoration(
-            //alignment: Alignment.center,
-            color: Colors.teal,
-            borderRadius: const BorderRadius.all(Radius.circular(4000) ),
-            border: Border.all(color: Colors.black, width: 10),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.blue,
-                offset: Offset(1, 1),
-                spreadRadius: 1,
-                blurRadius: 10,
-              ),
-            ],
-          ),
-          width: 300,
-          height: 300,
-          alignment: Alignment.center,
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(20),
-          child: const Text(
-            "Abdullah Subih",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30.2,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          child: ListView(children: [
+             Card(
+
+               //margin: EdgeInsets.only(bottom: 20),
+               color: Colors.teal,
+               child: ListTile(
+               leading: Text('1'),
+               /*onTap: (){
+                 print('on tap')
+               },*/
+               title: Text("abdullah"),
+               subtitle: Text('nickname : almah'),
+               trailing: Text('trailing'),
+             ),),
+            Card(
+              color: Colors.red,
+              child: ListTile(
+              leading: Text('2'),
+              /*onTap: (){
+                 print('on tap')
+               },*/
+              title: Text("saleh"),
+              subtitle: Text('nickname : leader'),
+              trailing: Text('trailing'),
+            ),)
+          ],),
         ),
       ),
     );
