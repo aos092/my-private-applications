9b81daa95d885812b9d0e1a9d21b3f03ed22686a
abdullah <aos772627092@gmail.com>
2026-01-31 05:22:14 +0300

    radio

--- lib/main.dart
+++ lib/main.dart
@@ -12,34 +12,76 @@ class MyApp extends StatefulWidget{
 }
 class _MyAppState extends State<MyApp>{
 
-  bool status = true;
+  String? country;
+  int? age;
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('StateFulWidget & SetState'),),
+        appBar: AppBar(title: const Text('RadioListTile & Radio'),),
         body: Container(
-
           padding: EdgeInsets.all(10),
           child: Column (
             children: [
-
-              IconButton(
-                  onPressed: (){
-                    setState(() {
-                      status = true;
-                    });
-              },
-              icon: Icon(Icons.add)),
-              status == true ? Icon(Icons.star): Icon(Icons.star_border_outlined),
-
-              IconButton(
-                onPressed: (){
+              Text('Choose Your Country' , style: TextStyle(fontSize: 20)),
+              RadioListTile(
+                title: Text('yemen'),
+                value: 'yemen',
+                groupValue: country,
+                onChanged: (val) {
                   setState(() {
-                    status = false;
+                   country = val;
                   });
-                },
-                icon: Icon(Icons.remove)),
+                }),
+              RadioListTile(
+                  title: Text('saudi arabia'),
+                  value: 'saudi arabia',
+                  groupValue: country,
+                  onChanged: (val) {
+                    setState(() {
+                      country = val;
+                    });
+                  }),
+              RadioListTile(
+                  title: Text('oman'),
+                  value: 'oman',
+                  groupValue: country,
+                  onChanged: (val) {
+                    setState(() {
+                      country = val;
+                    });
+                  }),
+
+              Text('Choose Your Age' , style: TextStyle(fontSize: 20)),
+              RadioListTile(
+                  title: Text('25'),
+                  value: 25,
+                  groupValue: age,
+                  onChanged: (val) {
+                    setState(() {
+                      age = val;
+                    });
+                  }),
+              RadioListTile(
+                  title: Text('18'),
+                  value: 18,
+                  groupValue: age,
+                  onChanged: (val) {
+                    setState(() {
+                      age = val;
+                    });
+                  }),
+              RadioListTile(
+                  title: Text('28'),
+                  value: 28,
+                  groupValue: age,
+                  onChanged: (val) {
+                    setState(() {
+                      age = val;
+                    });
+                  }),
+              Text('Your Country $country' ,style: TextStyle(fontSize: 20 ,color: Colors.red), ),
+              Text('Your Age $age' ,style: TextStyle(fontSize: 20 ,color: Colors.red), )
             ],
           ),
         ),
