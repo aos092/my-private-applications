1b8164df710e05f59feffbe028f7d683c4801708
abdullah <aos772627092@gmail.com>
2026-02-11 13:20:52 +0300

    v

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text("container widget")),
+        appBar: AppBar(
+          leading: IconButton(onPressed: (){}, icon: Icon(Icons.person)),
+          titleTextStyle: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),
+          actions: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.list))
+          ],
+          backgroundColor: Colors.orangeAccent,
+            title: const Text("abdullah")),
         body: Container(
-          decoration: BoxDecoration(
-            //alignment: Alignment.center,
-            color: Colors.teal,
-            borderRadius: const BorderRadius.all(Radius.circular(4000) ),
-            border: Border.all(color: Colors.black, width: 10),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.blue,
-                offset: Offset(1, 1),
-                spreadRadius: 1,
-                blurRadius: 10,
-              ),
-            ],
-          ),
-          width: 300,
-          height: 300,
-          alignment: Alignment.center,
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(20),
-          child: const Text(
-            "Abdullah Subih",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30.2,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+
         ),
       ),
     );
