2ad0e43361379fc13ee520729be738c287c90754
abdullah <aos772627092@gmail.com>
2026-02-16 20:12:47 +0600

    Tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -9,41 +9,54 @@ class MyApp extends StatefulWidget{
 
   @override
   State<MyApp> createState() => _MyAppState();
+
 }
 class _MyAppState extends State<MyApp>{
 
-  bool status = true;
+  GlobalKey<ScaffoldState> scaffoldkey = GlobalKey();
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('StateFulWidget & SetState'),),
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+          length: 4,
+          initialIndex: 1,
+          child: Scaffold (
+        key: scaffoldkey,
+        appBar: AppBar(
+          title:  Text('Tabbar'),
+          bottom: TabBar(
+              indicatorColor: Colors.deepOrange,
+              labelColor: Colors.deepOrange,
+              labelStyle: TextStyle(fontSize: 15),
+              unselectedLabelColor: Colors.black,
+              unselectedLabelStyle: TextStyle(fontSize: 10),
+              tabs: [
+          Tab(
+            icon: Icon(Icons.chat),
+            child: Text('Chat'),),
+          Tab(
+            icon: Icon(Icons.update),
+            child: Text('Updates'),),
+          Tab(
+            icon: Icon(Icons.commute),
+            child: Text('Communities'),),
+          Tab(
+            icon: Icon(Icons.call),
+            child: Text('Calls'),)
+        ]),),
         body: Container(
+         padding: EdgeInsets.all(10),
+          child: TabBarView(children: [
+            Text('Chat Page'),
+            Text('Updates Page'),
+            Text('Communities Page'),
+            Text('Calls Page')
 
-          padding: EdgeInsets.all(10),
-          child: Column (
-            children: [
-
-              IconButton(
-                  onPressed: (){
-                    setState(() {
-                      status = true;
-                    });
-              },
-              icon: Icon(Icons.add)),
-              status == true ? Icon(Icons.star): Icon(Icons.star_border_outlined),
-
-              IconButton(
-                onPressed: (){
-                  setState(() {
-                    status = false;
-                  });
-                },
-                icon: Icon(Icons.remove)),
-            ],
-          ),
+          ]),
+ 
         ),
-      ),
+      )),
     );
   }
 }
